bf56eb4d6d8949d2ad4ecd8bfec330b446b8e668
Sang Phan <plsang@nii.ac.jp>
2014-04-23 13:43:35 +0900

    modified gen metadata to support multiple settings without recalculating kernel...

--- calker_create_basic_exp.m
+++ calker_create_basic_exp.m
@@ -17,43 +17,76 @@ function calker_create_basic_exp_train_()
 	fprintf('Generating for TVMED13-PS...\n');
 	exp_prefix = 'TVMED13-PS';
 	event_nums = [6:15, 21:30];
-	calker_create_basic_exp_train_set(MEDMD, exp_prefix, event_nums, meta_dir);
+	calker_create_basic_exp_train_set(MEDMD, exp_prefix, event_nums, meta_dir, 'PS');
 
 	%% setting for MED 2013,
 	%% Ad-hoc tasks: 
 	fprintf('Generating for TVMED13-AH...\n');
 	exp_prefix = 'TVMED13-AH';
 	event_nums = [31:40];
-	calker_create_basic_exp_train_set(MEDMD, exp_prefix, event_nums, meta_dir);
+	calker_create_basic_exp_train_set(MEDMD, exp_prefix, event_nums, meta_dir, 'AH');
 	
 	%% setting for MED 2012,
 	%% Ad-hoc tasks: 
 	fprintf('Generating for TVMED12-AH...\n');
 	exp_prefix = 'TVMED12-AH';
 	event_nums = [16:20];
-	calker_create_basic_exp_train_set(MEDMD, exp_prefix, event_nums, meta_dir);
+	calker_create_basic_exp_train_set(MEDMD, exp_prefix, event_nums, meta_dir, 'AH');
 end
 
-function calker_create_basic_exp_train_set(MEDMD, exp_prefix, event_nums, meta_dir)
+function calker_create_basic_exp_train_set(MEDMD, exp_prefix, event_nums, meta_dir, event_type)
 
 	event_ids = arrayfun(@(x) sprintf('E%03d', x), event_nums, 'UniformOutput', false);
 	
-	%event_kits = {'EK10Ex', 'EK100Ex', 'EK130Ex'};
-	event_kits = {'EK10Ex', 'EK100Ex'};
+	if strcmp(event_type, 'PS'),
+		event_kits = {'EK10Ex', 'EK100Ex', 'EK130Ex'};
+	elseif strcmp(event_type, 'AH'),
+		event_kits = {'EK10Ex', 'EK100Ex'};
+	end
 	
 	%% 3 ways to use miss (related) videos
 	related_examples = {'RP', 'RN', 'NR'}; % RP: Related as Positive, RN: Related as Negative, NR: No Related 
 	
+	% universal database (specific for event types)
+	database = struct;
+	event_clip_names = {};
+	
 	for ii = 1:length(event_kits),
 		event_kit = event_kits{ii};
-		MEDMD.EventKit.(event_kit); 
 		
+		for jj = 1:length(event_ids),
+			event_id = event_ids{jj};
+			event_clip_names = [event_clip_names, MEDMD.EventKit.(event_kit).judge.(event_id).positive]; 
+			event_clip_names = [event_clip_names, MEDMD.EventKit.(event_kit).judge.(event_id).miss]; 
+		end
+	
+	end
+	
+	event_clip_names = unique(event_clip_names);
+		
+	bg_clip_names = unique(MEDMD.EventBG.default.clips);
+		
+	clip_names = [event_clip_names, bg_clip_names];
+		
+	clip_names = unique(clip_names);
+	clip_idxs = [1:length(clip_names)];
+	
+	%train_labels = repmat(init_labels, length(event_ids), 1);
+	
+	database.clip_names = clip_names;
+	database.clip_idxs = clip_idxs;
+	database.num_clip = length(clip_names);
+	database.event_ids = event_ids;
+	database.event_names = MEDMD.EventKit.(event_kit).eventnames(find(ismember(MEDMD.EventKit.(event_kit).eventids, event_ids)));
+	
+	train_labels = zeros(length(event_ids), length(clip_names));
+	
+	for ii = 1:length(event_kits),
+		event_kit = event_kits{ii};
 		exp_name = [exp_prefix, '-', event_kit];
 		
-		% as positive
-		database = struct;
-		
 		event_clip_names = {};
+		
 		for jj = 1:length(event_ids),
 			event_id = event_ids{jj};
 			event_clip_names = [event_clip_names, MEDMD.EventKit.(event_kit).judge.(event_id).positive]; 
@@ -67,21 +100,12 @@ function calker_create_basic_exp_train_set(MEDMD, exp_prefix, event_nums, meta_d
 		clip_names = [event_clip_names, bg_clip_names];
 		
 		clip_names = unique(clip_names);
-		clip_idxs = [1:length(clip_names)];
 		
-		init_labels = -ones(length(clip_names), 1);
-		
-		train_labels = repmat(init_labels, 1, length(event_ids));
-		
-		database.clip_names = clip_names;
-		database.clip_idxs = clip_idxs;
-		database.num_clip = length(clip_names);
-		database.event_ids = event_ids;
-		database.event_names = MEDMD.EventKit.(event_kit).eventnames(find(ismember(MEDMD.EventKit.(event_kit).eventids, event_ids)));
+		database.sel_idx = find(ismember(database.clip_names, clip_names));
 		
 		for kk = 1:length(related_examples),
 			r_example = related_examples{kk};
-			r_train_labels = train_labels;
+			r_train_labels = train_labels(:, database.sel_idx);
 			
 			r_exp_name = [exp_name, '-', r_example];
 			output_dir = sprintf('%s/%s', meta_dir, r_exp_name);
@@ -91,22 +115,25 @@ function calker_create_basic_exp_train_set(MEDMD, exp_prefix, event_nums, meta_d
 			if exist(output_file, 'file'), fprintf('File %s already exist!\n', output_file); continue; end;
 			
 			for jj = 1:length(event_ids),
+				
+				r_train_labels(jj, :) = -1;
+				
 				event_id = event_ids{jj};
 				event_pos_clips = MEDMD.EventKit.(event_kit).judge.(event_id).positive;
 				event_miss_clips = MEDMD.EventKit.(event_kit).judge.(event_id).miss;
 				
 				event_pos_idx = find(ismember(clip_names, event_pos_clips));
-				r_train_labels(event_pos_idx, jj) = 1;
+				r_train_labels(jj, event_pos_idx) = 1;
 				
 				event_miss_idx = find(ismember(clip_names, event_miss_clips));
 				
 				switch r_example,
 					case 'RP' 
-						r_train_labels(event_miss_idx, jj) = 1;
+						r_train_labels(jj, event_miss_idx) = 1;
 					case 'RN' 
-						r_train_labels(event_miss_idx, jj) = -1;
+						r_train_labels(jj, event_miss_idx) = -1;
 					case 'NR' 
-						r_train_labels(event_miss_idx, jj) = 0;
+						r_train_labels(jj, event_miss_idx) = 0;
 					otherwise 
 						error('Unknow related example type!');
 				end
