0d100ed980932ff8f99d70f33c0826293f03ca87
Sang Phan <plsang@nii.ac.jp>
2015-03-23 17:01:37 +0900

    support loading modes, (for testing modes, missing features will be all-zeros

--- calker_load_feature.m
+++ calker_load_feature.m
@@ -1,7 +1,10 @@
 
-function [feats, labels] = calker_load_feature(proj_name, exp_name, ker, video_pat)
+function [feats, labels] = calker_load_feature(proj_name, exp_name, ker, video_pat, mode)
 
     %% video_pat: bg, kindred14, medtest14, or an event id
+    %% mode: train (missing feature will be removed), other modes (test): missing features will be all-zero vectors
+    if ~exist('mode', 'var'),  mode = 'train'; end;
+    
 	configs = set_global_config();
 	logfile = sprintf('%s/%s.log', configs.logdir, mfilename);
 	msg = sprintf('Start running %s(%s, %s, %s)', mfilename, proj_name, exp_name);
@@ -12,16 +15,16 @@ function [feats, labels] = calker_load_feature(proj_name, exp_name, ker, video_p
     clips = []; 
     labels = []; %% only in case of event videos
     
-    if (any(ismember(MEDMD.EventKit.EK10Ex.eventids, video_pat))), 
+    if (any(ismember(ker.MEDMD.EventKit.EK10Ex.eventids, video_pat))), 
         if strcmp(ker.prms.rtype, 'NR'),
-            clips = [MEDMD.EventKit.(ker.prms.eventkit).judge.(video_pat).positive];
-            labels = ones(1, length(clips)));
+            clips = [ker.MEDMD.EventKit.(ker.prms.eventkit).judge.(video_pat).positive];
+            labels = ones(1, length(clips));
         else
-            clips = [MEDMD.EventKit.(ker.prms.eventkit).judge.(video_pat).positive, MEDMD.EventKit.(ker.prms.eventkit).judge.(video_pat).miss];
+            clips = [ker.MEDMD.EventKit.(ker.prms.eventkit).judge.(video_pat).positive, ker.MEDMD.EventKit.(ker.prms.eventkit).judge.(video_pat).miss];
             if strcmp(ker.prms.rtype, 'RP')
-                labels = [ones(1, length(clips)];
+                labels = ones(1, length(clips));
             elseif strcmp(ker.prms.rtype, 'RN')
-                labels = [ones(1, length(MEDMD.EventKit.(ker.prms.eventkit).judge.(video_pat).positive)), -ones(1, length(MEDMD.EventKit.(ker.prms.eventkit).judge.(video_pat).miss)];
+                labels = [ones(1, length(ker.MEDMD.EventKit.(ker.prms.eventkit).judge.(video_pat).positive)), -ones(1, length(ker.MEDMD.EventKit.(ker.prms.eventkit).judge.(video_pat).miss))];
             else
                 error('unknown how to use missed videos!!!\n');
             end
@@ -30,11 +33,11 @@ function [feats, labels] = calker_load_feature(proj_name, exp_name, ker, video_p
     else
         switch video_pat,
             case 'bg'
-                clips = MEDMD.EventBG.default.clips;
+                clips = ker.MEDMD.EventBG.default.clips;
             case 'kindred14'
-                clips = MEDMD.RefTest.KINDREDTEST.clips;
+                clips = ker.MEDMD.RefTest.KINDREDTEST.clips;
             case 'medtest14'
-                clips = MEDMD.RefTest.MEDTEST.clips;
+                clips = ker.MEDMD.RefTest.MEDTEST.clips;
             otherwise
                 error('unknown video pat!!!\n');
         end
@@ -48,7 +51,7 @@ function [feats, labels] = calker_load_feature(proj_name, exp_name, ker, video_p
         clip_name = clips{ii};
                                 
         segment_path = sprintf('%s/%s/feature/%s/%s/%s/%s.mat',...
-                        ker.proj_dir, proj_name, ker.prms.seg_name, ker.feat_raw, fileparts(ker.MEDMD.lookup.(clip_name)), clip_name);
+                        ker.proj_dir, proj_name, exp_name, ker.feat_raw, fileparts(ker.MEDMD.lookup.(clip_name)), clip_name);
                         
         if ~exist(segment_path),
             msg = sprintf('File [%s] does not exist!\n', segment_path);
@@ -68,7 +71,6 @@ function [feats, labels] = calker_load_feature(proj_name, exp_name, ker, video_p
         
         if size(code, 1) ~= ker.num_dim,
             fprintf('Dimension mismatch [%d-%d-%s]. Skipped !!\n', size(code, 1), ker.num_dim, segment_path);
-            size(code)
             continue;
         end
         
@@ -102,11 +104,14 @@ function [feats, labels] = calker_load_feature(proj_name, exp_name, ker, video_p
         
     end
 
-    sel_feat = selected_label ~= 0;
-    feats = feats(:, sel_feat);
-    
-    if ~isempty(labels),
-        labels = labels(sel_feat);
+    if strcmp(mode, 'train'),
+        sel_feat = selected_label ~= 0;
+        
+        feats = feats(:, sel_feat);
+        
+        if ~isempty(labels),
+            labels = labels(sel_feat);
+        end 
     end
     
 	elapsed = toc;
